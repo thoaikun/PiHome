303479925669f45528df5c63b9f3cbba47c7b7f5
Hùng Nguyễn Hoàng Bảo <hung.nhb@gmail.com>
2023-03-19 17:36:00 +0700

    re-organize socket and create env, door controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,25 @@
 import express, { Express } from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import EnvController from './app/controller/env.controller'
 
 import TestController from './app/controller/test.controller'
 import MqttClient from './utils/mqttClient'
+import Subscriber from './utils/subscriber'
 
 const port = 3000
 // const app: Express = express()
 // const httpServer = createServer(app)
 const io = new Server(3000)
 
-let testController = new TestController()
+// let testController = new TestController()
+let envController: Subscriber = new EnvController()
 
 const mqttClient: MqttClient = new MqttClient()
-mqttClient.subscribe(testController)
-mqttClient.subscribeTopic('thoaile/feeds/welcome-feed')
+// mqttClient.subscribe(testController)
+// mqttClient.subscribeTopic('thoaile/feeds/welcome-feed')
+mqttClient.subscribe(envController, 'env controller')
+mqttClient.subscribeTopic('thoaile/feeds/envstatus')
 
 // app.use(express.json())
 // app.use(express.urlencoded({ extended: false }))
@@ -26,10 +31,14 @@ mqttClient.subscribeTopic('thoaile/feeds/welcome-feed')
 
 // app.listen(port, () => console.log(`Server listen on port ${port}`))
 
+const controllerSocket = {}
+
 io.on('connection', (socket) => {
-    socket.on('join controller room', () => {
-        socket.join('controller room')
+    socket.on('join controller room', (message) => {
+        // socket.join('controller room')
+        controllerSocket[message] = socket
     })
+
     socket.on('join client room', () => {
         socket.join('client room')
 
@@ -42,8 +51,14 @@ io.on('connection', (socket) => {
             })
     })
 
-    socket.on('controller', (message) => {
+    socket.on('controller', (message: string) => {
         io.to('client room').emit('to client', message)
+    })
+
+    socket.on('client', (message: string) => {
+        // io.to('controller room').emit('to controller', message)
+        let data = JSON.parse(message)
+        controllerSocket[data.to].emit('to controller', message)
     })
 })
 
